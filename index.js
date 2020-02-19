16da156ba501d4251c88ca24f2ef274c395a4136
Eze Sunday Eze <mailsforeze@gmail.com>
2020-02-19 12:39:03 +0100

    fix(urlencode): add the extended option

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ database()
 
 // Middlewares for service request
 app.use(express.json())
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use(cors())
 
 // Routes
