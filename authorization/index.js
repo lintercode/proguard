d8eae7bb54f49f2ef5e01483e07b9cc0d8339100
Eze Sunday Eze <mailsforeze@gmail.com>
2020-03-10 16:23:26 +0100

    fix: remove mysql example

--- authorization/index.js
+++ authorization/index.js
@@ -1,20 +1,11 @@
 
 const { User } = require('../model')
 
+// Protects routes that requires logged access
 const isAuthorized = async (req, res, next)=>{
-    const username = req.session.username
-    
-    // for local authorization, you can use this middleware to check if a user is logged or not, if there are logged in moved to the next middleware
-    // Otherwise, redirect them to the login page
-    // basic example with mysql
-    // connection.query("SELECT username FROM users where email = ?", [email], (err, result)=>{
-    //     if (result.length>0){
-    //         next()
-    //     }else{
-    //         res.redirect("/admin/login")
-    //     }
-    // })
-  
+
+    // Assumming the email is in the session
+    const email = req.session.email
     // Another basic example with mongoose
     const data = await User.findOne({ email })
     if (data) {
